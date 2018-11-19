6b4c6aea8081ce9d8e513f2171c781550cb0e665
aakwah <aakwah@gmail.com>
2018-11-19 11:05:22 +0300

    FB works

--- FB_Stages/Stages_control.cs
+++ FB_Stages/Stages_control.cs
@@ -4,6 +4,8 @@ using System.Runtime.InteropServices;
 using System.Collections;
 using FB;
 using InSAT.Library.Interop;
+using System.IO;
+using System.Globalization;
 
 namespace FB_Stages
 {
@@ -14,6 +16,7 @@ namespace FB_Stages
 	 DisplayName("Стадийное управление"),
 	 ControllerCode(54),
 	 FBOptions(FBOptions.UseScanByTime)]
+	
 	public class Stages_control : StaticFBBase
 	{
 		const int Pin = 1;
@@ -31,24 +34,29 @@ namespace FB_Stages
 		const int Pout3 = 12;
 		const int Pout4 = 13;
 
-		double[,] stages = new double[100, 3];
+		static double[,] stages = new double[100, 3];
 		double[][,] rec = new double[10][,];//массив рецептов или массив массивов стадий
 		////////////////////////////////////////////////////////////ВХОДА
-		int in_stage_number;//номер стадии на входе фб
+		static int in_stage_number;//номер стадии на входе фб
 		double in_volume;// заданный объем на входе фб
 		double in_start_conc;//заданная начальная концентрация на входе фб
 		double in_final_conc;//заданная конечная концентрация на входе фб
-		int in_recipe_number;// номер рецепта
+		static int in_recipe_number;// номер рецепта
 		bool acpt_write;// разрешение на запись
 		bool recipe_write;// записать рецепт
 		bool recipe_read;// считать рецепт
-		////////////////////////////////////////////////////////////ВЫХОДА
+						 ////////////////////////////////////////////////////////////ВЫХОДА
 		int out_stage_number;//номер стадии на выходе фб
 		double out_volume;// заданный объем на выходе фб
 		double out_start_conc;//заданная начальная концентрация на выходе фб
 		double out_final_conc;//заданная конечная концентрация на выходе фб
 		int read_recipe;// считанный рецепт
 
+        static string path; //путь для папки
+		//public static double[,] mas = new double[10, 3];
+		static int rows = stages.GetUpperBound(0) + 1; //переменная для пересчета строк
+		static int columns = stages.Length / rows; //переменная для пересчета столбцов
+		//double[,] masout = new double[rows, columns];
 		void Write()//функциия записи входных параметров
 		{
 			if (acpt_write)
@@ -66,7 +74,7 @@ namespace FB_Stages
 			out_start_conc= stages[in_stage_number, 1];
 			out_final_conc = stages[in_stage_number, 2];
 		}
-		void Recipe()//функция обработки рецептов
+		/*void Recipe()//функция обработки рецептов
 		{
 			if (recipe_write)//запись в массив рецептов
 			{	for (int i=0; i<stages.GetLength(0); i++)
@@ -87,8 +95,63 @@ namespace FB_Stages
 					}
 				}
 			}
+		}*/
+		void SaveToFile()
+		{	
+			if (recipe_write)
+			{
+				string razdelenie = ";";  //переманная для разделения
+				string perenos = "\r\n"; //переменная для переноса
+				string specifier;  //переменная для преобразования значений в биты      
+				using (FileStream fs = File.Create(path)) //открытие пути файла
+				{
+					for (int i = 0; i < rows; i++) //пересчет строк
+					{
+						for (int j = 0; j < columns; j++) //пересчет столбцов в каждой строке
+						{
+							specifier = stages[i, j].ToString("G", CultureInfo.CreateSpecificCulture("fr-FR")); //преобразование каждого значения в строчный тип
+							byte[] array = System.Text.Encoding.Default.GetBytes(specifier); //преобразование строчного значения в битовое
+							if (j != 0) // условие для разделения значений
+							{
+								byte[] brzdl = System.Text.Encoding.Default.GetBytes(razdelenie); //преобразование разделителям в байты
+								fs.Write(brzdl, 0, brzdl.Length); //запись разделителя
+							}
+							fs.Write(array, 0, array.Length); //запись битового значения
+						}
+						byte[] bprns = System.Text.Encoding.Default.GetBytes(perenos); //преобразование переноса в биты
+						fs.Write(bprns, 0, bprns.Length); //запись переноса
+					}
+				}
+			}
+				
 		}
+		void DownloadFromFile()
+		{
+			if (recipe_read)
+			{
+				using (FileStream fs2 = File.OpenRead(path))         //начало описания алгоритма возврата значений в матрицу
+				{
+					byte[] arrayout = new byte[fs2.Length];
+					fs2.Read(arrayout, 0, arrayout.Length);  //битовые значения из файла
+					string textFromFile = System.Text.Encoding.Default.GetString(arrayout); //преобразования битовых значений в string
+					textFromFile = textFromFile.Replace("\r\n", ";"); //корректировка переносов & разделений
+					string[] words = textFromFile.Split(new char[] { ';' }); //разбиение значений в массив
+					int q = 0; //ввод счетчика считываемого массива
+					for (int y = 0; y < rows; y++) //пересчет строк
+					{
+						for (int x = 0; x < columns; x++) //пересчет столбцов
+						{
+							double t = Convert.ToDouble(words[q]); //преобразование переменной в double
+							stages[y, x] = t; // присвоение массиву
+											  //Console.WriteLine(masout[x, y]);
+							q += 1;
+						}
+					}
 
+				}
+			}
+			
+		}
 		protected override void UpdateData()
         {
 			Out();
@@ -107,6 +170,9 @@ namespace FB_Stages
 			SetPinValue(Pout3, out_stage_number);
 			SetPinValue(Pout4, read_recipe);
 			Write();
+			path = string.Format(@"c:\note\note{0}.txt", Convert.ToString(in_recipe_number));
+			SaveToFile();
+			DownloadFromFile();
 		}
 		
 	}
